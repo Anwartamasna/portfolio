db6f22653f07ab0b7eff92e7ee5b4384580d935a
Anwartamasna <tamasna.anwar@gmail.com>
2024-08-20 08:25:13 +0100

    full project

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,5 @@
 <?php
-  $receiving_email_address = 'glad.ma@outlook.com';
+  $receiving_email_address = 'tamasna.anwar@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
